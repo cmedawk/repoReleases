610352b6c3efdc644bee15d2e8d188635a09ee79
cmedaK <cesarpenadosli@gmail.com>
2025-09-20 21:36:51 -0600

    primer lanzamiento para este paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoReleases",                            # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Cesar Meda",                         # Tu nombre
+    author_email="cesarmeda.wk@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/cmedawk/repoReleases.git",     # URL del proyecto
+)
